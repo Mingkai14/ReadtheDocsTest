6284eb8776ebd0b78769d024b038ac79334ad8ab
geralt14 <wmkkkk@126.com>
2024-07-03 07:05:12 +0100

    first commit

--- Docs/conf.py
+++ Docs/conf.py
@@ -25,6 +25,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "default"
+
